b815c47018c29279550a5a4a4e700058fd3fbcd5
GeorgeNebulishvili <g.nebulishvili@iset.ge>
2023-02-20 00:11:30 +0400

    new version

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const moon = document.querySelector("#moon");
 const sun = document.querySelector("#sun");
 const title = document.querySelector(".title");
@@ -6,10 +5,25 @@ const themeTextDark = document.querySelector(".theme-text-dark");
 const themeTextLight = document.querySelector(".theme-text-light");
 const userinfo = document.querySelector(".user-info");
 const searchinput = document.querySelector("#user");
-const searchform = document.querySelector (".form");
+const card = document.querySelector (".card");
 const button = document.querySelector(".btn");
 const userplcholder = document.querySelector("#user::placeholder");
+const avatarMobile  = document.querySelector(".avatar-mobile");
+const avatarDesktop = document.querySelector(".avatar-desktop");
+const nameuser = document.querySelector(".name");
+const login = document.querySelector (".login");
+const joinDate = document.querySelector (".join-date");
+const bio = document.querySelector (".bio");
+const repos = document.querySelector ("#repos");
+const followers = document.querySelector ("#followers");
+const following = document.querySelector ("#following");
+const city = document.querySelector ("#city");
+const blog = document.querySelector ("#blog");
+const twitter = document.querySelector ("#twitter");
+const company = document.querySelector ("#company");
+const errorelement = document.querySelector (".error");
 
+// კონკრეტული იუზერის მონაცემები
 const octocat   = {
     avatar_url: "https://avatars.githubusercontent.com/u/583231?v=4",
     bio: null,
@@ -43,9 +57,67 @@ const octocat   = {
     type: "User",
     updated_at: "2023-01-22T12:13:51Z",
     url: "https://api.github.com/users/octocat",
+    };
+
+// ერორის ტექსტის გასუფთავება ელემენტის ცვლილებაზე-წაშლა(input-არის ივენთი)
+searchinput.addEventListener("input", () => {
+    errorelement.textContent = "";
+});
+
+// თარიღის ტრანსფორმაცია სასურველ ფორმატში
+const dateTransfomer = (date) =>   {
+    const dateobj = new Date(date);
+    const dateString = dateobj.toDateString();
+    const [weekday, month, day, year] = dateString.split(" ");
+    return `${day} ${month} ${year}`;
+    // console.log(weekday, month, day, year);
 }
+// იუზერის ინფორმაციის შევსება
+const displayInfo = (user) => {
+    avatarMobile.src = user.avatar_url;
+    avatarDesktop.src = user.avatar_url;
+    nameuser.textContent = user.name;
+    login.textContent = "@" + user.login;
+    const date = dateTransfomer(user.created_at);
+    joinDate.textContent = " Joined " + date;
+    bio.textContent = 
+        user.bio || 
+        "მშვიდობა ყოველთააა..."
+    repos.textContent = user.public_repos;
+    followers.textContent = user.followers;
+    following.textContent = user.following;
+    if(user.location)   {
+        city.textContent = user.location;
+    } else {
+        city.textContent = "Not Available";
+        city.parentElement.style.opacity = 0.5;
+    }
+    if(user.twitter_username)   {
+        twitter.textContent = user.twitter_username;
+    } else {
+        twitter.textContent = "Not Available";
+        twitter.parentElement.style.opacity = 0.5;
+    }
+    if(user.blog)   {
+        blog.textContent = user.blog;
+        blog.href = user.blog;
+    } else {
+        blog.textContent = "Not Available";
+        blog.parentElement.style.opacity = 0.5;
+        blog.href = "#";
+    }
+    if(user.company)   {
+        company.textContent = user.company;
+    } else {
+        company.textContent = "Not Available";
+        company.parentElement.style.opacity = 0.5;
+    }
 
+    };
 
+displayInfo (octocat);
+
+// თემის შეცვლა (dark; light)
 const fliptheme = (theme) => {
     if (theme === "dark")  { 
         moon.style.display = "none";
@@ -53,7 +125,8 @@ const fliptheme = (theme) => {
         themeTextLight.style.display = "block";
         themeTextDark.style.display ="none"
         userinfo.style.backgroundColor = "#1E2A47";
-        searchform.style.backgroundColor = "#1E2A47";
+        card.style.backgroundColor = "#1E2A47";
+        // stats.style.backgroundColor = "#141d2f";
         document.body.style.backgroundColor = "#141d2f";
         
         } else {
@@ -63,7 +136,8 @@ const fliptheme = (theme) => {
         themeTextDark.classList.toggle("dark");
         userinfo.classList.toggle ("dark");
         searchinput.classList.toggle  ("dark");
-        searchform.classList.toggle ("dark");
+        card.classList.toggle ("dark");
+        Array.from(card).forEach(card =>card.classList.toggle("dark"));
         
     if (theme === "light")  {
         moon.style.display = "block";
@@ -71,7 +145,7 @@ const fliptheme = (theme) => {
         themeTextDark.style.display = "block";
         themeTextLight.style.display = "none"
         userinfo.style.backgroundColor = "white";
-        searchform.style.backgroundColor = "white";
+        card.style.backgroundColor = "white";
         document.body.style.backgroundColor = "#ffffff";
         } else {
 
@@ -80,7 +154,7 @@ const fliptheme = (theme) => {
         themeTextDark.classList.toggle("light");
         userinfo.classList.toggle ("light");
     }
-
+// კლიკზე შეუცვალოს კლასი
 moon.addEventListener("click", () => fliptheme("dark"));
 sun.addEventListener("click", () => fliptheme("light"));
 
@@ -89,17 +163,18 @@ sun.addEventListener("click", () => fliptheme("light"));
 //     console.log (e.target.value);
 //  });
 
+// იუზერის ძებნის ივენთი
 button.addEventListener("click", async (event) => {
     event.preventDefault();
     try {
      const response = await axios.get("https://api.github.com/users/" + searchinput.value
      );
      const user = response.data;
+     displayInfo(user);
      searchinput.value=""; 
-     console.log(user);
     } catch (error) {
         console.log (error);
+        errorelement.textContent = "No result"
     }
     
 });
-
